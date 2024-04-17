814a69d5708b5eb8baed487aeb5699e9e7259066
deepesh5560 <dssharma912@gmail.com>
2024-04-17 22:22:17 +0530

    new changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */ const imagesDomains =
   process?.env?.NEXT_PUBLIC_BASE_API_URL;
-const nextConfig = { images: { domains: ['https://land-listing-psi.vercel.app/'] } };
+const nextConfig = { images: { domains: ['https://land-listing-psi.vercel.app','land-listing-rb5buy7wm-deepeshs-projects-78006c6a.vercel.app'] } };
 module.exports = nextConfig;
