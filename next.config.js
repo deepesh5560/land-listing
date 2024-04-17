f75ef6c697198f371661998325be7d8987494ea1
deepesh5560 <dssharma912@gmail.com>
2024-04-17 22:18:22 +0530

    config change

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */ const imagesDomains =
   process?.env?.NEXT_PUBLIC_BASE_API_URL;
-const nextConfig = { images: { domains: 'https://land-listing-psi.vercel.app/' } };
+const nextConfig = { images: { domains: ['https://land-listing-psi.vercel.app/'] } };
 module.exports = nextConfig;
